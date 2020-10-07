8649eeadea1dd6e690e16b0c555ef408610ce76b
YI-MING Chen <borischen0203@gmail.com>
2020-10-08 02:50:09 +0800

    update solution58

---
+++ src/solution58.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+/**
+ * 
+ * Given a string s consists of upper/lower-case alphabets and empty space
+ * characters ' ', return the length of last word (last word means the last
+ * appearing word if we loop from left to right) in the string.
+ * 
+ * If the last word does not exist, return 0.
+ * 
+ * Note: A word is defined as a maximal substring consisting of non-space
+ * characters only.
+ * 
+ * Example:
+ * 
+ * Input: "Hello World"
+ * 
+ * Output: 5
+ */
+
+public class solution58 {
+    public int lengthOfLastWord(String s) {
+        String[] ss = s.split("\\s+");
+        if (ss.length == 0) {
+            return 0;
+        } else {
+            int lastIndex = ss.length - 1;
+            return ss[lastIndex].length();
+        }
+    }
+
+    // public int lengthOfLastWord(String s) {
+    // String ss = s.trim();
+    // if (ss.length() == 0) {
+    // return 0;
+    // } else {
+    // int lastIndex = ss.length - 1;
+    // return ss[lastIndex].length();
+    // }
+    // }
+
+    public static void main(String[] args) {
+        solution58 s = new solution58();
+        String ss = "                  ";
+        String[] sa = ss.split("\\s+");
+        // System.out.println(sa.length);
+        // System.out.println(Arrays.toString(sa));
+        System.out.println(s.lengthOfLastWord("h             "));
+    }
+}
